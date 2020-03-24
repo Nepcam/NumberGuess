22d7b9b36cf1d83df7a247eba179b209eed9433b
Nepcam <cameron.nepe@gmail.com>
2020-03-24 23:30:06 +1300

    Set app info, and changed up hte console font colors

--- NumberGuess/Program.cs
+++ NumberGuess/Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 
+// Namespace
 namespace NumberGuess
 {
+    // Main Class
     class Program
     {
+        // Entry Point Method
         static void Main(string[] args)
         {
+            // Set up vars
+            string appName = "Number Guess";
+            string appVersion = "1.0.0";
+            string appAuthor = "Cameron Nepe";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // Reset text color
+            Console.ResetColor();
         }
     }
 }
